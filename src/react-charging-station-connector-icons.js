26194f1876363ed9d43bbf67cc2155d8b5609587
Jennifer Kirsch <jennifer.kirsch@binary-butterfly.de>
2021-05-19 17:42:42 +0200

    Init
    Remove react-create-app bloat and add initial code

---
+++ src/react-charging-station-connector-icons.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import ChademoIcon from '../charging-station-connector-icons/solid/CHADEMO.svg';
+import ChademoSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/CHADEMO.svg';
+import DomesticFIcon from '../charging-station-connector-icons/solid/DOMESTIC_F.svg';
+import DomesticFSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/DOMESTIC_F.svg';
+import IEC603092SingleIcon from '../charging-station-connector-icons/solid/IEC_60309_2_single.svg';
+import IEC603092SingleSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_60309_2_single.svg';
+import IEC603092SThreeIcon from '../charging-station-connector-icons/solid/IEC_60309_2_three.svg';
+import IEC603092SThreeSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_60309_2_three.svg';
+import IEC62196T1Icon from '../charging-station-connector-icons/solid/IEC_62196_T1.svg';
+import IEC62196T1SubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T1.svg';
+import IEC62196T2Icon from '../charging-station-connector-icons/solid/IEC_62196_T2.svg';
+import IEC62196T2SubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T2.svg';
+import IEC62196T2ComboIcon from '../charging-station-connector-icons/solid/IEC_62196_T2_COMBO.svg';
+import IEC62196T2ComboSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T2_COMBO.svg';
+import TeslaSIcon from '../charging-station-connector-icons/solid/TESLA_S.svg';
+import TeslaSSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/TESLA_S.svg';
+
+export const Chademo = (props) => {
+    return <ChademoIcon {...props}/>;
+};
+export const ChademoSubtitled = (props) => {
+    return <ChademoSubtitledIcon {...props}/>;
+};
+
+export const DomesticF = (props) => {
+    return <DomesticFIcon {...props}/>;
+};
+
+export const DomesticFSubtitled = (props) => {
+    return <DomesticFSubtitledIcon/>;
+};
+
+export const IEC603092Single = (props) => {
+    return <IEC603092SingleIcon {...props}/>;
+};
+
+export const IEC603092SingleSubtitled = (props) => {
+    return <IEC603092SingleSubtitledIcon {...props}/>;
+};
+
+export const IEC603092SThree = (props) => {
+    return <IEC603092SThreeIcon {...props}/>;
+};
+
+export const IEC603092SThreeSubtitled = (props) => {
+    return <IEC603092SThreeSubtitledIcon {...props} />;
+};
+
+export const IEC62196T1 = (props) => {
+    return <IEC62196T1Icon {...props}/>;
+};
+
+export const IEC62196T1Subtitled = (props) => {
+    return <IEC62196T1SubtitledIcon {...props}/>;
+};
+
+export const IEC62196T2 = (props) => {
+    return <IEC62196T2Icon {...props}/>;
+};
+
+export const IEC62196T2Subtitled = (props) => {
+    return <IEC62196T2SubtitledIcon {...props}/>;
+};
+
+export const IEC62196T2Combo = (props) => {
+    return <IEC62196T2ComboIcon {...props}/>;
+};
+
+export const IEC62196T2ComboSubtitled = (props) => {
+    return <IEC62196T2ComboSubtitledIcon {...props}/>;
+};
+
+export const TeslaS = (props) => {
+    return <TeslaSIcon {...props}/>;
+};
+
+export const TeslaSSubtitled = (props) => {
+    return <TeslaSSubtitledIcon {...props}/>;
+};
