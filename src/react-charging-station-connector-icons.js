397dad2266db905c4c8f9778af3534167ab4098d
Jennifer Kirsch <jennifer.kirsch@binary-butterfly.de>
2021-05-20 10:26:11 +0200

    Make the way icons work a bit less verbose
    Also add light variants

--- src/react-charging-station-connector-icons.js
+++ src/react-charging-station-connector-icons.js
@@ -1,81 +1,131 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-import ChademoIcon from '../charging-station-connector-icons/solid/CHADEMO.svg';
-import ChademoSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/CHADEMO.svg';
-import DomesticFIcon from '../charging-station-connector-icons/solid/DOMESTIC_F.svg';
-import DomesticFSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/DOMESTIC_F.svg';
-import IEC603092SingleIcon from '../charging-station-connector-icons/solid/IEC_60309_2_single.svg';
-import IEC603092SingleSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_60309_2_single.svg';
-import IEC603092SThreeIcon from '../charging-station-connector-icons/solid/IEC_60309_2_three.svg';
-import IEC603092SThreeSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_60309_2_three.svg';
-import IEC62196T1Icon from '../charging-station-connector-icons/solid/IEC_62196_T1.svg';
-import IEC62196T1SubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T1.svg';
-import IEC62196T2Icon from '../charging-station-connector-icons/solid/IEC_62196_T2.svg';
-import IEC62196T2SubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T2.svg';
-import IEC62196T2ComboIcon from '../charging-station-connector-icons/solid/IEC_62196_T2_COMBO.svg';
-import IEC62196T2ComboSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T2_COMBO.svg';
-import TeslaSIcon from '../charging-station-connector-icons/solid/TESLA_S.svg';
-import TeslaSSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/TESLA_S.svg';
+import ChademoSolidIcon from '../charging-station-connector-icons/solid/CHADEMO.svg';
+import ChademoSolidSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/CHADEMO.svg';
+import ChademoLightIcon from '../charging-station-connector-icons/light/CHADEMO.svg';
+import ChademoLightSubtitledIcon from '../charging-station-connector-icons/light-subtitled/CHADEMO.svg';
+import DomesticFSolidIcon from '../charging-station-connector-icons/solid/DOMESTIC_F.svg';
+import DomesticFSolidSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/DOMESTIC_F.svg';
+import DomesticFLightIcon from '../charging-station-connector-icons/light/DOMESTIC_F.svg';
+import DomesticFLightSubtitledIcon from '../charging-station-connector-icons/light-subtitled/DOMESTIC_F.svg';
+import IEC603092SingleSolidIcon from '../charging-station-connector-icons/solid/IEC_60309_2_single.svg';
+import IEC603092SingleSolidSubtitledIcon
+    from '../charging-station-connector-icons/solid-subtitled/IEC_60309_2_single.svg';
+import IEC603092SingleLightIcon from '../charging-station-connector-icons/light/IEC_60309_2_single.svg';
+import IEC603092SingleLightSubtitledIcon
+    from '../charging-station-connector-icons/light-subtitled/IEC_60309_2_single.svg';
+import IEC603092SThreeSolidIcon from '../charging-station-connector-icons/solid/IEC_60309_2_three.svg';
+import IEC603092SThreeSolidSubtitledIcon
+    from '../charging-station-connector-icons/solid-subtitled/IEC_60309_2_three.svg';
+import IEC603092SThreeLightIcon from '../charging-station-connector-icons/light/IEC_60309_2_three.svg';
+import IEC603092SThreeLightSubtitledIcon
+    from '../charging-station-connector-icons/light-subtitled/IEC_60309_2_three.svg';
+import IEC62196T1SolidIcon from '../charging-station-connector-icons/solid/IEC_62196_T1.svg';
+import IEC62196T1SolidSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T1.svg';
+import IEC62196T1LightIcon from '../charging-station-connector-icons/light/IEC_62196_T1.svg';
+import IEC62196T1LightSubtitledIcon from '../charging-station-connector-icons/light-subtitled/IEC_62196_T1.svg';
+import IEC62196T2SolidIcon from '../charging-station-connector-icons/solid/IEC_62196_T2.svg';
+import IEC62196T2SolidSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T2.svg';
+import IEC62196T2LightIcon from '../charging-station-connector-icons/light/IEC_62196_T2.svg';
+import IEC62196T2LightSubtitledIcon from '../charging-station-connector-icons/light-subtitled/IEC_62196_T2.svg';
+import IEC62196T2ComboSolidIcon from '../charging-station-connector-icons/solid/IEC_62196_T2_COMBO.svg';
+import IEC62196T2ComboSolidSubtitledIcon
+    from '../charging-station-connector-icons/solid-subtitled/IEC_62196_T2_COMBO.svg';
+import IEC62196T2ComboLightIcon from '../charging-station-connector-icons/light/IEC_62196_T2_COMBO.svg';
+import IEC62196T2ComboLightSubtitledIcon
+    from '../charging-station-connector-icons/light-subtitled/IEC_62196_T2_COMBO.svg';
+import TeslaSSolidIcon from '../charging-station-connector-icons/solid/TESLA_S.svg';
+import TeslaSSolidSubtitledIcon from '../charging-station-connector-icons/solid-subtitled/TESLA_S.svg';
+import TeslaSLightIcon from '../charging-station-connector-icons/light/TESLA_S.svg';
+import TeslaSLightSubtitledIcon from '../charging-station-connector-icons/light-subtitled/TESLA_S.svg';
+
+
+const showIcon = (variants, props, variant, subtitled) => {
+    let Ret;
+    if (variant === 'solid') {
+        Ret = subtitled ? variants['solid']['subtitled'] : variants['solid']['default'];
+    } else if (variant === 'light') {
+        Ret = subtitled ? variants['light']['subtitled'] : variants['light']['default'];
+    } else {
+        throw Error('Unsupported variant: ' + variant);
+    }
+    return <Ret {...props}/>;
+};
+
+const iconPropTypes = {
+    variant: PropTypes.oneOf(['solid', 'light']).isRequired,
+    subtitled: PropTypes.bool.isRequired,
+};
 
 export const Chademo = (props) => {
-    return <ChademoIcon {...props}/>;
+    return showIcon({
+        solid: {default: ChademoSolidIcon, subtitled: ChademoSolidSubtitledIcon},
+        light: {default: ChademoLightIcon, subtitled: ChademoLightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
-export const ChademoSubtitled = (props) => {
-    return <ChademoSubtitledIcon {...props}/>;
-};
+
+Chademo.propTypes = iconPropTypes;
 
 export const DomesticF = (props) => {
-    return <DomesticFIcon {...props}/>;
+    return showIcon({
+        solid: {default: DomesticFSolidIcon, subtitled: DomesticFSolidSubtitledIcon},
+        light: {default: DomesticFLightIcon, subtitled: DomesticFLightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const DomesticFSubtitled = (props) => {
-    return <DomesticFSubtitledIcon/>;
-};
+DomesticF.propTypes = iconPropTypes;
 
 export const IEC603092Single = (props) => {
-    return <IEC603092SingleIcon {...props}/>;
+    return showIcon({
+        solid: {default: IEC603092SingleSolidIcon, subtitled: IEC603092SingleSolidSubtitledIcon},
+        light: {default: IEC603092SingleLightIcon, subtitled: IEC603092SingleLightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const IEC603092SingleSubtitled = (props) => {
-    return <IEC603092SingleSubtitledIcon {...props}/>;
-};
+IEC603092Single.propTypes = iconPropTypes;
 
 export const IEC603092SThree = (props) => {
-    return <IEC603092SThreeIcon {...props}/>;
+    return showIcon({
+        solid: {default: IEC603092SThreeSolidIcon, subtitled: IEC603092SThreeSolidSubtitledIcon},
+        light: {default: IEC603092SThreeLightIcon, subtitled: IEC603092SThreeLightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const IEC603092SThreeSubtitled = (props) => {
-    return <IEC603092SThreeSubtitledIcon {...props} />;
-};
+IEC603092SThree.propTypes = iconPropTypes;
 
 export const IEC62196T1 = (props) => {
-    return <IEC62196T1Icon {...props}/>;
+    return showIcon({
+        solid: {default: IEC62196T1SolidIcon, subtitled: IEC62196T1SolidSubtitledIcon},
+        light: {default: IEC62196T1LightIcon, subtitled: IEC62196T1LightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const IEC62196T1Subtitled = (props) => {
-    return <IEC62196T1SubtitledIcon {...props}/>;
-};
+IEC62196T1.propTypes = iconPropTypes;
 
 export const IEC62196T2 = (props) => {
-    return <IEC62196T2Icon {...props}/>;
+    return showIcon({
+        solid: {default: IEC62196T2SolidIcon, subtitled: IEC62196T2SolidSubtitledIcon},
+        light: {default: IEC62196T2LightIcon, subtitled: IEC62196T2LightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const IEC62196T2Subtitled = (props) => {
-    return <IEC62196T2SubtitledIcon {...props}/>;
-};
+IEC62196T2.propTypes = iconPropTypes;
 
 export const IEC62196T2Combo = (props) => {
-    return <IEC62196T2ComboIcon {...props}/>;
+    return showIcon({
+        solid: {default: IEC62196T2ComboSolidIcon, subtitled: IEC62196T2ComboSolidSubtitledIcon},
+        light: {default: IEC62196T2ComboLightIcon, subtitled: IEC62196T2ComboLightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const IEC62196T2ComboSubtitled = (props) => {
-    return <IEC62196T2ComboSubtitledIcon {...props}/>;
-};
+IEC62196T2Combo.propTypes = iconPropTypes;
 
 export const TeslaS = (props) => {
-    return <TeslaSIcon {...props}/>;
+    return showIcon({
+        solid: {default: TeslaSSolidIcon, subtitled: TeslaSSolidSubtitledIcon},
+        light: {default: TeslaSLightIcon, subtitled: TeslaSLightSubtitledIcon},
+    }, props, props.variant, props.subtitled);
 };
 
-export const TeslaSSubtitled = (props) => {
-    return <TeslaSSubtitledIcon {...props}/>;
-};
+TeslaS.propTypes = iconPropTypes;
